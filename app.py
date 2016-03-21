15945b4a9203992b1902639a40156a852a202e76
Timopheym <timopheym@gmail.com>
2016-03-21 20:13:05 +1000

    added request_get

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import json
 
 app = Flask(__name__)
 
@@ -7,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '{"status": true}'
+
+@app.route('/submit_get')
+def submit_get():
+    return json.dumps(request.args)
 
 @app.route('/submit', methods=['POST'])
 def submit():
