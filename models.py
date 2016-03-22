8a3f709bda902d8cfd1f0e306638e8835bd47b96
carxsing <mr.kur.anton@gmail.com>
2016-03-22 17:42:40 +1000

    added files

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask.ext.mongoalchemy import MongoAlchemy
+app = Flask(__name__)
+app.config['MONGOALCHEMY_DATABASE'] = 'library'
+db = MongoAlchemy(app)
+
+class Mesurement(db.Document):
+    temp = db.FloatField()
+    ground_temp = db.FloatField()
+    light = db.FloatField()
+    wind_speed = db.FloatField()
+    wind_direction = db.FloatField()
+    pressure = db.FloatField()
+    humidity = db.FloatField()
+
+class Sign(db.Document):
+    sign = db.StringField()
+
+class Clothes(db.Document):
+    temp = db.FloatField()
+    name = db.StringField()
